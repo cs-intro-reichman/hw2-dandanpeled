697b99f7bc4bee1c77d6541436d0079e502cdacc
Dan Peled <danpeled@Dans-MacBook-Air.local>
2024-11-21 15:14:04 +0200

    CalcLPi

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,41 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+
+		int number_of_numbers = Integer.parseInt(args[0]);
+		double pi_calc = 1.0;
+		double divider = 1.0;
+
+		for (double i = 1.0; i < number_of_numbers ; i++){
+			System.out.println("Num i: " +i);
+			divider = divider+2;
+			System.out.println("Divider num: " +divider);
+
+			System.out.println("Divider: "+ (1/divider));
+			pi_calc = pi_calc - 1/divider;
+			System.out.println(pi_calc );
+
+			
+			i++;
+			if (number_of_numbers == i){
+				break;
+			}
+			
+			System.out.println("Num i: " +i);
+			divider = divider+2;
+			System.out.println("Divider num: " +divider);
+
+			System.out.println("Divider: "+ (1/divider));
+			pi_calc = pi_calc + 1/divider;
+			System.out.println(pi_calc);
+			System.out.println("Num i: " +i);
+
+
+			System.out.println("=============");
+
+		}
+
+		System.out.println(pi_calc * 4);
+	
 	}
 }
--- Cheers.java
+++ Cheers.java
@@ -1,16 +1,21 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
+            // Input from user
 	    String word = args[0];
+
+            // Variables
             char letter;
             int repetition = Integer.parseInt(args[1]);
             String testChar = "aAeEfFhHiIlLmMnNoOrRsSxX";
             String output = "";
             
+            // Loop through all the input
             for (int i = 0; i < word.length() ; i++){
-                letter = word.charAt(i);
+                letter = word.charAt(i); // Get the letter
                 String letter_str = Character.toString(letter).toUpperCase();
-
+                
+                // Check if the letter need to get a or an
                 if (testChar.indexOf(letter) != -1){
                         
                         output = "Give me an " + letter_str + ": " + letter_str + "!";
@@ -25,6 +30,7 @@ public class Cheers {
             }
             System.out.println("What does that spell?");
 
+            //Output loop of the input word
             for (int i = 0; i < repetition; i++){
                 System.out.println(word.toUpperCase() + "!!!");
             }
